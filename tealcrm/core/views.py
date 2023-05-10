1324fb1c2ce84dc37ebb2bdc4fdbd0d9ada2bcfd
Aditya Singh <adibetu13@gmail.com>
2023-05-10 20:44:09 +0530

    final

--- tealcrm/core/views.py
+++ tealcrm/core/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'core/index.html')
 
-
 def about(request):
     return render(request, 'core/about.html')
--- tealcrm/userprofile/views.py
+++ tealcrm/userprofile/views.py
@@ -1,24 +1,32 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-from django.shortcuts import render,redirect
-
+from .forms import SignupForm
 from .models import Userprofile
 
-def signup(request):
+from team.models import Team
 
+def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
+
         if form.is_valid():
             user = form.save()
-            
-            Userprofile.objects.create(user=user)
 
+            team = Team.objects.create(name='The team name', created_by=user)
+            team.members.add(user)
+            team.save()
+            
+            Userprofile.objects.create(user=user, active_team=team)
 
             return redirect('/log-in/')
-        
     else:
+        form = SignupForm()
 
-    
-        form = UserCreationForm()
+    return render(request, 'userprofile/signup.html', {
+        'form': form
+    })
 
-    return render(request, 'userprofile/signup.html',{'form':form})
+@login_required
+def myaccount(request):
+    return render(request, 'userprofile/myaccount.html')
